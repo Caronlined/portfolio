756d816f8af4881b54b9fc375fc6fa8b13ac9681
Pablo Marbian <pablomarbian@btsws34md6r.fibertel.com.ar>
2021-09-14 10:23:24 -0300

    image fix

--- src/elements/portfolio/PortfolioList.jsx
+++ src/elements/portfolio/PortfolioList.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-const Portfolio_image = <img src="/assets/images/portfolio/portfolio-1.jpg" alt="Recent works images" />;
-const Portfolio_image2 = <img src="/assets/images/portfolio/portfolio-2.jpg" alt="Recent works images" />;
-const Portfolio_image3 = <img src="/assets/images/portfolio/portfolio-3.jpg" alt="Recent works images" />;
-const Portfolio_image4 = <img src="/assets/images/portfolio/portfolio-4.jpg" alt="Recent works images" />;
-const Portfolio_image5 = <img src="/assets/images/portfolio/portfolio-5.jpg" alt="Recent works images" />;
-const Portfolio_image6 = <img src="/assets/images/portfolio/portfolio-6.jpg" alt="Recent works images" />;
-const Portfolio_image7 = <img src="/assets/images/portfolio/portfolio-7.jpg" alt="Recent works images" />;
-const Portfolio_image8 = <img src="/assets/images/portfolio/portfolio-8.jpg" alt="Recent works images" />;
-const Portfolio_image9 = <img src="/assets/images/portfolio/portfolio-9.jpg" alt="Recent works images" />;
+const Portfolio_image = <img src="homepagePath/assets/images/portfolio/portfolio-1.jpg" alt="Recent works images" />;
+const Portfolio_image2 = <img src="homepagePath/assets/images/portfolio/portfolio-2.jpg" alt="Recent works images" />;
+const Portfolio_image3 = <img src="homepagePath/assets/images/portfolio/portfolio-3.jpg" alt="Recent works images" />;
+const Portfolio_image4 = <img src="homepagePath/assets/images/portfolio/portfolio-4.jpg" alt="Recent works images" />;
+const Portfolio_image5 = <img src="homepagePath/assets/images/portfolio/portfolio-5.jpg" alt="Recent works images" />;
+const Portfolio_image6 = <img src="homepagePathassets/images/portfolio/portfolio-6.jpg" alt="Recent works images" />;
+const Portfolio_image7 = <img src="homepagePath/assets/images/portfolio/portfolio-7.jpg" alt="Recent works images" />;
+const Portfolio_image8 = <img src="homepagePath/assets/images/portfolio/portfolio-8.jpg" alt="Recent works images" />;
+const Portfolio_image9 = <img src="homepagePath/assets/images/portfolio/portfolio-9.jpg" alt="Recent works images" />;
 
 const PortfolioListContent = [
     {
