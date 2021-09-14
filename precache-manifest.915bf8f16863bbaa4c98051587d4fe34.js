56b0e69a140d59cf3dfd6876603e6f5dbb168eec
Pablo Marbian <pablomarbian@btsws34md6r.fibertel.com.ar>
2021-09-14 09:45:39 -0300

    Updates

---
+++ precache-manifest.915bf8f16863bbaa4c98051587d4fe34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168fc492b43adec2a0834d608d1816c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "452bbf768f78cd485696",
+    "url": "/portfolio/static/css/2.d6bc2e59.chunk.css"
+  },
+  {
+    "revision": "44dc3f2e8c9256b91131",
+    "url": "/portfolio/static/css/main.c3370ded.chunk.css"
+  },
+  {
+    "revision": "452bbf768f78cd485696",
+    "url": "/portfolio/static/js/2.b3c791fa.chunk.js"
+  },
+  {
+    "revision": "4effa9de81938b2545f40d4f7c3a8025",
+    "url": "/portfolio/static/js/2.b3c791fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dc3f2e8c9256b91131",
+    "url": "/portfolio/static/js/main.710aa9c8.chunk.js"
+  },
+  {
+    "revision": "3ec4eb0c28d114d41526",
+    "url": "/portfolio/static/js/runtime~main.c73a5873.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
